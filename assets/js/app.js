0bc016d7acfe06a74c30b95c727b561a46d277ad
MCavieres <maca.c.91@gmail.com>
2024-03-21 15:30:54 -0300

    agregado el metodo setTimeout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,25 @@
 const result = document.querySelector("#result");
 
-const albumes = async () => {
-    try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/photos");
-        const data = await response.json();
+const albumes = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(async () => {
+            try {
+                const response = await fetch("https://jsonplaceholder.typicode.com/photos");
+                const data = await response.json();
 
-        for (let i = 0; i < 20; i++) {
-            console.log(data[i].title);
-            let li = document.createElement("li");
-            result.appendChild(li);
-            li.textContent = `${data[i].title}`;
-        }
-    } catch (err) {
-        console.log(err);
-    }
+                for (let i = 0; i < 20; i++) {
+                    // console.log(data[i].title);
+                    let li = document.createElement("li");
+                    result.appendChild(li);
+                    li.textContent = `${data[i].title}`;
+                }
+
+                resolve(console.log(`Informacion enviada`));
+            } catch (err) {
+                reject(console.log(err));
+            }
+        }, 3000);
+    });
 };
 
 albumes();
