2945811a6cc3b401fc2e82e12aa7b139a4d90a62
MCavieres <maca.c.91@gmail.com>
2024-03-21 16:06:13 -0300

    creacion de 3 funciones por separdo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,64 @@
 const result = document.querySelector("#result");
 
-const albumes = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(async () => {
-            try {
-                const response = await fetch("https://jsonplaceholder.typicode.com/photos");
-                const data = await response.json();
+// const albumes = () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(async () => {
+//             try {
+//                 const response = await fetch("https://jsonplaceholder.typicode.com/photos");
+//                 const data = await response.json();
 
-                for (let i = 0; i < 20; i++) {
-                    // console.log(data[i].title);
-                    let li = document.createElement("li");
-                    result.appendChild(li);
-                    li.textContent = `${data[i].title}`;
-                }
+//                 for (let i = 0; i < 20; i++) {
+//                     // console.log(data[i].title);
+//                     let li = document.createElement("li");
+//                     result.appendChild(li);
+//                     li.textContent = `${data[i].title}`;
+//                 }
 
-                resolve(console.log(`Informacion enviada`));
-            } catch (err) {
-                reject(`Error por ${err}`);
-            }
+//                 resolve(console.log(`Informacion enviada`));
+//             } catch (err) {
+//                 reject(`Error por ${err}`);
+//             }
+//         }, 3000);
+//     });
+// };
+
+// albumes();
+
+const albumes = async () => {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/photos");
+        const data = await response.json();
+
+        for (let i = 0; i < 20; i++) {
+            let li = document.createElement("li");
+            result.appendChild(li);
+            li.textContent = `${data[i].title}`;
+        }
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+// albumes();
+
+const tresSegundos = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Informacion enviada");
         }, 3000);
     });
 };
 
-albumes();
+const msg = async () => {
+    try {
+        await albumes();
+        const answer = await tresSegundos();
+        if (albumes()) {
+            console.log(answer);
+        }
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+msg();
